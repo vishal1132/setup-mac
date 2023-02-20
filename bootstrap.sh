f9a710d94f656aa7bc2082684793bddd666077fd
vishal1132 <vishals@iitbhilai.ac.in>
2023-02-20 12:43:11 +0530

    clone dotfiles private repo and stow to create symlinks in /Users/vishalsharma

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,3 +8,7 @@ sudo xcodebuild -license
 brew install ansible
 ansible-galaxy install -r requirements.yml
 ansible-playbook -i "localhost," -c local dev-playbook.yml --ask-become-pass
+
+# once all the apps are installed now need to clone the dotfiles repo to $HOME/.config
+git clone git@github.com:vishal1132/dotfiles.git $HOME/.config
+cd $HOME/.config && stow .
